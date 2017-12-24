b7a39962a2dc6f671dabe6c0b79d84e2d4d32e59
jeremybrooks <jeremyb@whirljack.net>
2017-12-24 15:39:13 -0800

    Added copyright

--- app/src/main/java/net/jeremybrooks/readsy/activites/ReadsyActivity.java
+++ app/src/main/java/net/jeremybrooks/readsy/activites/ReadsyActivity.java
@@ -1,3 +1,25 @@
+/*
+ * readsy - read something new every day <http://jeremybrooks.net/readsy>
+ *
+ * Copyright (c) 2017  Jeremy Brooks
+ *
+ * This file is part of readsy for Android.
+ *
+ * readsy for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * readsy for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with readsy for Android.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package net.jeremybrooks.readsy.activites;
 
 import android.content.Context;
